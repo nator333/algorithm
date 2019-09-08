6aefcc48b14255c1a8d9903cda2e3cab8ff7fbcd
nator333 <m.nakamata35@gmail.com>
2019-09-07 19:55:57 -0700

    Update Regex

--- java/src/main/java/algorithm/leetcode/RegularExpressionMatching.java
+++ java/src/main/java/algorithm/leetcode/RegularExpressionMatching.java
@@ -2,6 +2,16 @@ package algorithm.leetcode;
 
 public class RegularExpressionMatching {
   public boolean isMatch(String s, String p) {
-    return true;
+    if (p.isEmpty()) {
+      return s.isEmpty();
+    }
+
+    boolean isMatch = (!s.isEmpty() && s.charAt(0) == p.charAt(0)) || p.charAt(0) == '.';
+    if (p.length() >= 2 && p.charAt(1) == '*'){
+      return (isMatch(s, p.substring(2)) ||
+          (isMatch && isMatch(s.substring(1), p)));
+    } else {
+      return isMatch && isMatch(s.substring(1), p.substring(1));
+    }
   }
 }
--- java/src/test/java/algorithm/leetcode/RegularExpressionMatchingTest.groovy
+++ java/src/test/java/algorithm/leetcode/RegularExpressionMatchingTest.groovy
@@ -3,9 +3,19 @@ package algorithm.leetcode
 class RegularExpressionMatchingTest extends GroovyTestCase {
     void testIsMatch() {
         def regularExpressionMatching = new RegularExpressionMatching();
+        assertEquals(true, regularExpressionMatching.isMatch("ab", "ab"))
+        assertEquals(true, regularExpressionMatching.isMatch("ab", ".."))
         assertEquals(true, regularExpressionMatching.isMatch("aa", "a*"))
         assertEquals(true, regularExpressionMatching.isMatch("ab", ".*"))
         assertEquals(true, regularExpressionMatching.isMatch("aab", "c*a*b"))
         assertEquals(false, regularExpressionMatching.isMatch("mississippi", "mis*is*p*."))
+        assertEquals(false, regularExpressionMatching.isMatch("aa", "a"))
+        assertEquals(false, regularExpressionMatching.isMatch("a", ""))
+        assertEquals(true, regularExpressionMatching.isMatch("aab", "a*b"))
+        assertEquals(true, regularExpressionMatching.isMatch("iss", "s*is*"))
+        assertEquals(true, regularExpressionMatching.isMatch("i", "s*is*"))
+        assertEquals(true, regularExpressionMatching.isMatch("ssiss", "s*is*"))
+        assertEquals(true, regularExpressionMatching.isMatch("ssissippi", "s*is*ip*."))
+        assertEquals(true, regularExpressionMatching.isMatch("ab", ".*c"))
     }
 }
